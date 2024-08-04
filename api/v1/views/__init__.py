fe63d4e1f560615987a958ecb7e8c2f612aca846
harper71 <princeazuka99@gmail.com>
2024-08-04 18:43:24 +0100

    make files executable

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,10 @@
 """
 initialize router views
 """
-
-
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
+
 
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.states import *
